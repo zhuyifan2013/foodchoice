c65fc6b0422ca1c96128ed3c5811b292299a11a4
Yifan <zhu_yifan@me.com>
2014-11-14 21:28:28 +0800

    init repo
    Signed-off-by: Yifan <zhu_yifan@me.com>

---
+++ src/com/mi/FoodChoice/Constants.java
@@ -0,0 +1,10 @@
+package com.mi.FoodChoice;
+
+public class Constants {
+
+    public final static int MAKE_CHOICE = 0;
+
+    public final static int EDIT_FOOD_SHOP = 1;
+
+    public final static int SETTING = 2;
+}
---
+++ src/com/mi/FoodChoice/MyActivity.java
@@ -0,0 +1,104 @@
+package com.mi.FoodChoice;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+public class MyActivity extends Activity {
+    private String[] mPlaneTitles;
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawer_main);
+        initActionBar();
+        initDrawerLayout();
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initActionBar() {
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        tintManager.setNavigationBarTintEnabled(true);
+        tintManager.setTintColor(Color.parseColor(getString(R.string.actionbar_color)));
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.actionbar_bg));
+        }
+    }
+
+    private void initDrawerLayout(){
+        mPlaneTitles = getResources().getStringArray(R.array.drawer_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        mDrawerList.setAdapter(
+                new ArrayAdapter<String>(this, R.layout.drawer_list_item, mPlaneTitles));
+
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer,
+                R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    public class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        switch (position) {
+            case Constants.MAKE_CHOICE:
+                mDrawerList.setItemChecked(position, true);
+                mDrawerLayout.closeDrawer();
+        }
+    }
+}
